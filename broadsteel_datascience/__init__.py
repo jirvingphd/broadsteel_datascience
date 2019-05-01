a92899e1de8b30fb9e68dce48c75070af5451ba0
James Irving <james.irving.phd@outlook.com>
2019-05-01 11:38:08 -0400

    init and broadsteel_datascience.py properly filled

--- broadsteel_datascience/__init__.py
+++ broadsteel_datascience/__init__.py
@@ -2,6 +2,7 @@
 
 """Top-level package for BroadSteel DatasScience."""
 
-__author__ = """James Irving"""
+__author__ = """James Irving, Michael Moravetz"""
 __email__ = 'james.irving.phd@outlook.com'
 __version__ = '0.1.0'
+from .broadsteel_datascience import *
